b8a5bdf3403df75f044eeeee04d6319c84c8c0dc
Mauricio Bernuy <mauricio.bernuy@utec.edu.pe>
2022-07-12 22:01:15 -0500

    some tests

--- tests/setuptable_test.py
+++ tests/setuptable_test.py
@@ -1,6 +1,6 @@
 from time import sleep
 import src.setuptable as setuptable
-
+import pytest
 
 success_msg = 'ok'
 error_msg = 'fail' 
@@ -15,10 +15,30 @@ def test_my_server():  #pragma: no cover
 
     def test_1():    
         assert try_test() == success_msg
+    # def success_msg():
+    #     try:
+    #         setuptable.addmessage("pepe","pepo")
 
+    def addmessage_passes():
+        try:
+            with pytest.raises(Exception) as excinfo:
+                setuptable.addmessage("pepe","pepo")
+            assert excinfo.value.args[0] == 'err'
+        except:
+            assert True
+            
+    def getmessage_passes():
+        try:
+            with pytest.raises(Exception) as excinfo:
+                setuptable.getmessagesbytopic("pepo")
+            assert excinfo.value.args[0] == 'err'
+        except:
+            assert True
     
     try:
         test_1()
+        addmessage_passes()
+        getmessage_passes()
     
     finally:
         print("ok!")
