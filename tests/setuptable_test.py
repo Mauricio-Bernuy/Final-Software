af455472a224589f407d19540b71d0c6dea1d417
Mauricio Bernuy <mauricio.bernuy@utec.edu.pe>
2022-07-12 22:07:26 -0500

    removed

--- tests/setuptable_test.py
+++ tests/setuptable_test.py
@@ -15,54 +15,9 @@ def test_my_server():  #pragma: no cover
 
     def test_1():    
         assert try_test() == success_msg
-    # def success_msg():
-    #     try:
-    #         setuptable.addmessage("pepe","pepo")
-
-    def addmessage_passes():
-        try:
-            with pytest.raises(Exception) as excinfo:
-                setuptable.addmessage("pepe","pepo")
-            assert excinfo.value.args[0] == 'err'
-        except:
-            assert True
-            
-    def getmessage_passes():
-        try:
-            with pytest.raises(Exception) as excinfo:
-                setuptable.getmessagesbytopic("pepo")
-            assert excinfo.value.args[0] == 'err'
-        except:
-            assert True
-
-    def createtable_exist_passes():
-        try:
-            with pytest.raises(Exception) as excinfo:
-                setuptable.createtable()
-            assert excinfo.value.args[0] == 'err'
-        except:
-            assert True
-
-    def droptable_passes():
-        try:
-            with pytest.raises(Exception) as excinfo:
-                setuptable.createtable()
-            assert excinfo.value.args[0] == 'err'
-        except:
-            assert True
-            
-    def createtable_new_passes():
-        try:
-            with pytest.raises(Exception) as excinfo:
-                setuptable.createtable()
-            assert excinfo.value.args[0] == 'err'
-        except:
-            assert True
-    
+   
     try:
         test_1()
-        addmessage_passes()
-        getmessage_passes()
         
     
     finally:
